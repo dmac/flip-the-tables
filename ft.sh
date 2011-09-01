a9d62242d866e323a70fc758ac1ab81edc070403
Caleb Spare <cespare@gmail.com>
2011-09-01 14:30:30 -0700

    Support project-specific rubies and a default ruby

--- ft.sh
+++ ft.sh
@@ -18,10 +18,10 @@
 #   $3 the precise string to be removed/replaced
 #   $4 the replacement string (use "" for removal)
 replace_path() {
-  local path=$1
-  local list=$2
-  local remove=$3
-  local replace=$4 # Allowed to be empty or unset
+  local path="$1"
+  local list="$2"
+  local remove="$3"
+  local replace="$4" # Allowed to be empty or unset
   export $path="$(echo "$list" | tr ":" "\n" | sed "s:^$remove\$:$replace:" | tr "\n" ":" | sed 's|:$||')"
 }
 
@@ -37,6 +37,25 @@ if [[ -n "$GEM_HOME" || -n "$GEM_PATH" ]]; then
   return 1
 fi
 
+if [[ -z "$FT_DEFAULT_RUBY" ]]; then
+  echo '$FT_DEFAULT_RUBY must be set to use flip-the-tables.'
+  return 1
+fi
+
+# Push the flip-the-tables ruby to the front of the path
+_ft_default_ruby=( $(find "$RUBIES" -type d -maxdepth 1 -name "${FT_DEFAULT_RUBY}*") )
+if [[ ${#_ft_default_ruby[@]} -eq 0 ]]; then
+  echo "Error: the default ruby \$FT_DEFAULT_RUBY ($FT_DEFAULT_RUBY) doesn't match any known rubies."
+  return 1
+elif [[ ${#_ft_default_ruby[@]} -gt 1 ]]; then
+  echo "Error: the default ruby \$FT_DEFAULT_RUBY ($FT_DEFAULT_RUBY) is ambiguous: it matches each of"
+  for r in "${_ft_default_ruby[@]}"; do
+    echo "$r"
+  done
+  return 1
+fi
+export PATH="${_ft_default_ruby[0]}/bin:$PATH"
+
 # Get the full list of versions
 _ft_ruby_list() {
   echo $(for f in $(find "$RUBIES" -type d -d 1); do basename "$f"; done)
@@ -44,22 +63,57 @@ _ft_ruby_list() {
 
 # Swap out ruby versions by replacing $RUBIES/<ruby1>/bin with $RUBIES/<ruby2>/bin
 _ft_set_ruby() {
-  local current=$1
-  local pattern=$2
-  local ruby=( $(find "$RUBIES" -type d -d 1 -name "${pattern}*") )
+  local current="$1"
+  local pattern="$2"
+  if [[ -z "$current" ]]; then
+    current="$(echo "$PATH" | tr ":" "\n" | grep -m 1 "^$RUBIES/.*/bin/\?\$")"
+  fi
+  if [[ -z "$pattern" ]]; then
+    pattern="$FT_DEFAULT_RUBY"
+  fi
+  local ruby=( $(find "$RUBIES" -type d -maxdepth 1 -name "${pattern}*") )
   if [[ ${#ruby[@]} -eq 0 ]]; then
     echo "Error: No ruby matched $pattern."
   elif [[ ${#ruby[@]} -gt 1 ]]; then
     echo "Error: ambiguous ruby \"$pattern\": the following rubies all matched"
     local r
-    for r in ${ruby[@]}; do
+    for r in "${ruby[@]}"; do
       basename "$r"
     done
   elif [[ "$(dirname "$current")" != "${ruby[0]}" ]]; then
-    echo -e "\033[01;32mSwitching to ruby $(basename "$ruby").\033[39m"
+    echo -e "\033[01;32mNow using ruby $(basename "$ruby").\033[39m"
     replace_path PATH "$PATH" "$current" "$ruby/bin"
   fi
 }
+
+_ft_set_from_project_file() {
+  local project_files=( $(find "$1" -type f -maxdepth 1 -name "\.ft_ruby_*") )
+  if [[ ${#project_files[@]} -eq 0 ]]; then
+    if [[ "$1" = "/" ]]; then
+      _ft_set_ruby
+    else
+      _ft_set_from_project_file "$(dirname "$1")"
+    fi
+  elif [[ ${#project_files[@]} -eq 1 ]]; then
+    local file="$(basename "${project_files[0]}")"
+    echo "Using flip-the-tables project file $1/$file"
+    _ft_set_ruby "" "${file#\.ft_ruby_}"
+  else # > 1 project files
+    echo "Error: more than 1 matching flip-the-tables project files found in $1:"
+    for f in "${project_files[@]}"; do
+      echo "$f"
+    done
+  fi
+}
+
+_ft_prompt_command() {
+  if [[ "${_ft_cwd}" != "$(pwd)" ]]; then
+    export _ft_cwd="$(pwd)"
+    _ft_set_from_project_file "${_ft_cwd}"
+  fi
+}
+
+export PROMPT_COMMAND='_ft_prompt_command;'"$PROMPT_COMMAND"
 
 _ft_help() {
   echo 'flip-the-tables: easily switch ruby paths around.'
@@ -86,8 +140,8 @@ ft() {
   if [[ "$#" -ne 1 ]]; then
     _ft_help
   else
-    local current=$(echo "$PATH" | tr ":" "\n" | grep -m 1 "^$RUBIES/.*/bin/\?\$")
-    local current_short=$(basename $(dirname "$current"))
+    local current="$(echo "$PATH" | tr ":" "\n" | grep -m 1 "^$RUBIES/.*/bin/\?\$")"
+    local current_short="$(basename $(dirname "$current"))"
     if [[ -z "$current" ]]; then
       echo 'Error: not currently using Ruby in $RUBIES.'
     else
